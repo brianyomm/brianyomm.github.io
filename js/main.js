4d003349f6436ce266694f9a9c40ca9a75a3f9c6
brianyomm <brian@noblees.com>
2015-08-19 13:46:45 -0400

    fixed fade animation

--- js/main.js
+++ js/main.js
@@ -55,20 +55,23 @@ $(window).on("load",function() {
     function fade() {
         $('.fade').each(function() {
             /* Check the location of each desired element */
+
             var objectBottom = $(this).offset().top + $(this).outerHeight();
             var windowBottom = $(window).scrollTop() + $(window).innerHeight();
-            
+
             /* If the object is completely visible in the window, fade it in */
             if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-                if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
+                if ($(this).css('opacity')==0) {$(this).stop(true,true).fadeTo("fast",1);}
             } else { //object goes out of view (scrolling up)
-                if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
+                if ($(this).css('opacity')==1) {$(this).stop(true,true).fadeTo("fast",0);}
             }
         });
     }
     fade(); //Fade in completely visible elements during page-load
     $(window).scroll(function() {fade();}); //Fade in elements during scroll
 });
+
+
 
 // add button show more content
 function toggler(divId) {
