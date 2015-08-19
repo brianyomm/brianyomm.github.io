e7bfad01585f5eba69b8c86fea9326f592cbb9cf
peterhwang2010 <inhohwang2010@gmail.com>
2015-08-19 13:19:16 -0400

    made fade work

--- js/main.js
+++ js/main.js
@@ -55,14 +55,15 @@ $(window).on("load",function() {
     function fade() {
         $('.fade').each(function() {
             /* Check the location of each desired element */
+
             var objectBottom = $(this).offset().top + $(this).outerHeight();
             var windowBottom = $(window).scrollTop() + $(window).innerHeight();
-            
+
             /* If the object is completely visible in the window, fade it in */
             if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-                if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
+                if ($(this).css('opacity')==0) {$(this).stop(true,true).fadeTo("fast",1);}
             } else { //object goes out of view (scrolling up)
-                if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
+                if ($(this).css('opacity')==1) {$(this).stop(true,true).fadeTo("fast",0);}
             }
         });
     }
