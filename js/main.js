d2dcce47cf8bd75841e777fe63b1976e91a42977
brianyomm <brian@noblees.com>
2015-08-24 12:30:27 -0400

    organize portfolio.html + add project

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-(function($) {
-    "use strict"; // Start of use strict
-    // add page-scroll
-    $('page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-})(jQuery); // End of use strict
+// (function($) {
+//     "use strict"; // Start of use strict
+//     // add page-scroll
+//     $('page-scroll').bind('click', function(event) {
+//         var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: ($($anchor.attr('href')).offset().top - 50)
+//         }, 1250, 'easeInOutExpo');
+//         event.preventDefault();
+//     });
+// })(jQuery); // End of use strict
 
 
 // add smooth scrolling
